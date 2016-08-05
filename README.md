5d852f81f65a6663c2349cdcabade00b147d2194
fmakdemir <fmakdemir@gmail.com>
2016-08-05 16:46:46 +0300

    added custom configs

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # pyColoredLogger
 Colored Logger library for python
+
+get a logger with:
+
+```
+from coloredlogger import ColoredLogger
+logger = ColoredLogger()
+```
+
+log using pre-setup methods:
+
+```
+logger.error('A red error')
+logger.success('A green success message')
+logger.info('A blue info message')
+logger.log('Or just some verbose log')
+```
+
+or make your own log method using a name prefix color level and whether only
+header will be colored or the whole line:
+
+```
+logger.add_config('my-log', {'prefix': "ROCK!",'color': ColoredLogger.COLORS.CYAN, 'header-only': True})
+logger.custom('my-log', 'YOU!')
+logger.custom('my-log', 'ALL!')
+```
+
--- coloredlogger.py
+++ coloredlogger.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python3
 from colorama import Fore, Back, Style
+import copy
 
 class ColoredLogger(object):
 	COLORS = Fore
+	_DEFAULT_CFG = {'level': 10, 'prefix': '[ ]', 'color': COLORS.WHITE, 'header-only': False}
 	def __init__(self):
 		self.configs = {
-			'error':{'level': 1, 'prefix': '[-]', 'color': self.COLORS.RED},
-			'info':{'level': 2, 'prefix': '[?]', 'color': self.COLORS.BLUE},
-			'success':{'level': 3, 'prefix': '[+]', 'color': self.COLORS.GREEN},
-			'verbose':{'level': 4, 'prefix': '[ ]', 'color': self.COLORS.WHITE},
+			'error':{'level': 1, 'prefix': '[-]', 'color': self.COLORS.RED, 'header-only': False},
+			'info':{'level': 2, 'prefix': '[?]', 'color': self.COLORS.BLUE, 'header-only': False},
+			'success':{'level': 3, 'prefix': '[+]', 'color': self.COLORS.GREEN, 'header-only': False},
+			'verbose':{'level': 4, 'prefix': '[ ]', 'color': self.COLORS.WHITE, 'header-only': False},
 		}
+
+	def add_config(self, config_name, config):
+		if type(config_name) != str or type(config) != dict:
+			raise TypeError('config_name is not str or config is not dict')
+
+		cfg = copy.deepcopy(self._DEFAULT_CFG)
+		for k in cfg.keys():
+			if k in config:
+				cfg[k] = config[k]
+
+		self.configs[config_name] = config
 
 	def _color_print(self, cfg_name, *args, **kwargs):
 		cfg = self.configs[cfg_name]
-		print(cfg['color'] + cfg['prefix'], ' '.join(args), Style.RESET_ALL)
+		header_suffix = ''
+		if cfg['header-only']:
+			header_suffix = self.COLORS.WHITE
+		print(cfg['color'] + cfg['prefix'] + header_suffix, ' '.join(args), Style.RESET_ALL)
 
 	def error(self, *args, **kwargs):
 		self._color_print('error', *args, **kwargs)
@@ -27,6 +43,8 @@ class ColoredLogger(object):
 	def log(self, *args, **kwargs):
 		self._color_print('verbose', *args, **kwargs)
 
+	def custom(self, *args, **kwargs):
+		self._color_print(args[0], *args[1:], **kwargs)
 
 if __name__ == '__main__':
 	logger = ColoredLogger()
@@ -34,3 +52,7 @@ if __name__ == '__main__':
 	logger.success('Such success much green wow')
 	logger.info('just a blue info')
 	logger.log('some log here')
+	# make your own logger mode
+	logger.add_config('my-log', {'prefix': "ROCK!",'color': ColoredLogger.COLORS.CYAN, 'header-only': True})
+	logger.custom('my-log', 'YOU!')
+	logger.custom('my-log', 'ALL!')
