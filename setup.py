ec8113f18e3217154230eae5203d9a00b12b287f
Fma <info@fmakdemir.com>
2017-04-24 17:02:43 +0300

    added python 3.6

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.3.4',
+    version='1.3.5',
     description='Colored Logger for Python',
     long_description=open(path.join(here, "README.rst")).read(),
     # The project's main homepage.
@@ -38,6 +38,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='coloredlogger logger log colored linux color colour coloured',
     py_modules=["coloredlogger"],
